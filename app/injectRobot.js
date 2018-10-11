6491f9696e538b62623020a485407e0cbb55eb77
Mendelssohn <Mendelssohn@DESKTOP-OAMJH0J>
2018-10-10 23:25:59 -0300

    update

--- app/injectRobot.js
+++ app/injectRobot.js
@@ -1,32 +1,31 @@
+window.name = 'NG_ENABLE_DEBUG_INFO!'
+
+var urlServer = "https://mendelssohntw.github.io/robot.tw2/app/";
+
 if(!window.loadedJs){
 	window.loadedJs = []
 }
 
-var urlServer = "https://mendelssohntw.github.io/robot.tw2/app/";
-var i = 0;
 if (!window.inject){
-	window.inject = function (url, dependencies, callback) {
+	window.inject = function inject(url, dependencies, callback) {
 		try {
 			var dependencies_loaded = true;
-
-			if(dependencies.length){
-				for (var d in dependencies) {
-					if(dependencies.hasOwnProperty(d)){
-						if (!loadedJs.some(f => f.toLowerCase() == dependencies[d].toLowerCase())) {
-							dependencies_loaded = false;
-							if (i <= 60) {
-								i++;
-								setTimeout(inject, 1000, dependencies[d], [], callback);
-							} else {
-								return;
-							}
-						}
+			var i = 0;
+			for (var d in dependencies) {
+				if (!loadedJs.some(f => f.toLowerCase() === dependencies[d].toLowerCase())) {
+					dependencies_loaded = false;
+					if (i <= 60) {
+						setTimeout(inject, 1000, dependencies[d], [], callback);
+					} else {
+						return;
 					}
 				}
+				i++;
 			}
 			if (dependencies_loaded) {
 				var scr = document.createElement("script");
 				scr.type = "text/javascript";
+//				scr.src = chrome.extension.getURL(url);
 				scr.src = url;
 				(document.head || document.body || document.documentElement).appendChild(scr);
 				loadedJs.push(url);
@@ -38,95 +37,95 @@ if (!window.inject){
 	};
 }
 
-if (!window.injectHTML){
-	window.injectHTML = function injectHTML(urls) {
-
-		function next(){
-			var url = urls.shift();
-			try {
-				var scr = document.createElement("div");
-				scr.setAttribute("ng-include", "");
-				scr.setAttribute("style", "height:100%");
-				scr.setAttribute("src", "'" + url + "'");
-				(document.head || document.body || document.documentElement).appendChild(scr);
-				if(urls.length){next()}
-			} catch (error) {
-				urls.unshift(url);
-				setTimeout(injectHTML, 1000, urls);
-				if(urls.length){next()}
-			}
-		}
-		next()
-	};
-}
-
-
-
-if (!window.checkHTML){
-	window.checkHTML = function () {
-		if (document.getElementById("interface-bottom")) {var scr = document.createElement("link");
-		scr.setAttribute("rel", "stylesheet");
-		scr.setAttribute("type", "text/css");
-		scr.setAttribute("href", "https://mendelssohntw.github.io/robot.tw2/app/css/mainrobot.css");
-
-			document.head.appendChild(scr)
-			
-			injectHTML([
-				urlServer + "view/main.html", 
-				urlServer + "view/headquarter.html", 
-				urlServer + "view/farm.html",
-				urlServer + "view/alert.html",
-				urlServer + "view/recruit.html",
-				urlServer + "view/attack.html",
-				urlServer + "view/defense.html"
-				]);
-		} else {
-			setTimeout(checkHTML, 1000);
-		}
-	}
-	checkHTML();
-}
+//if (!window.injectHTML){
+//	window.injectHTML = function injectHTML(urls) {
+//
+//		function next(){
+//			var url = urls.shift();
+//			try {
+//				var scr = document.createElement("div");
+//				scr.setAttribute("ng-include", "");
+//				scr.setAttribute("style", "height:100%");
+//				scr.setAttribute("src", "'" + chrome.extension.getURL(url) + "'");
+//				(document.head || document.body || document.documentElement).appendChild(scr);
+//				if(urls.length){next()}
+//			} catch (error) {
+//				urls.unshift(url);
+//				setTimeout(injectHTML, 1000, urls);
+//				if(urls.length){next()}
+//			}
+//		}
+//		next()
+//	};
+//}
+//
+//if (!window.checkHTML){
+//	window.checkHTML = function () {
+//		if (document.getElementById("interface-bottom")) {
+//			var scr = document.createElement("link");
+//			scr.setAttribute("rel", "stylesheet");
+//			scr.setAttribute("type", "text/css");
+//			scr.setAttribute("href", "https://mendelssohntw.github.io/robot.tw2/app/css/mainrobot.css");
+//			document.head.appendChild(scr);
+//			injectHTML([
+//				"view/main.html", 
+//				"view/headquarter.html", 
+//				"view/farm.html",
+//				"view/alert.html",
+//				"view/recruit.html",
+//				"view/attack.html",
+//				"view/attackcompletion.html",
+//				"view/defense.html"
+//				]);
+//		} else {
+//			setTimeout(checkHTML, 1000);
+//		}
+//	}
+//	checkHTML();
+//}
 
 if (!window.check){
 	window.check = function () {
 		if (document.getElementById("interface-bottom")) {
-			setTimeout(inject, 1000, urlServer + "js/json.js", [], function(){
-				setTimeout(inject, 1000, urlServer + "js/services.js", [], function(){
-					setTimeout(inject, 1000, urlServer + "js/ready.js", [urlServer + "js/services.js"], function(){
-						setTimeout(inject, 1000, urlServer + "js/database.js", [urlServer + "js/ready.js"], function(){
-							setTimeout(inject, 1000, urlServer + "js/utils.js", [urlServer + "js/database.js"], function(){
-								setTimeout(inject, 1000, urlServer + "js/builderWindow.js", [urlServer + "js/utils.js"], function(){
-									setTimeout(inject, 1000, urlServer + "js/headquarter.js", [urlServer + "js/builderWindow.js"], function(){
-										setTimeout(inject, 1000, urlServer + "js/alert.js", [urlServer + "js/builderWindow.js"], function(){
-											setTimeout(inject, 1000, urlServer + "js/recon.js", [urlServer + "js/builderWindow.js"], function(){
-												setTimeout(inject, 1000, urlServer + "js/spy.js", [urlServer + "js/builderWindow.js"], function(){
-													setTimeout(inject, 1000, urlServer + "js/attack.js", [urlServer + "js/builderWindow.js"], function(){
-														setTimeout(inject, 1000, urlServer + "js/defense.js", [urlServer + "js/builderWindow.js"], function(){
-															setTimeout(inject, 1000, urlServer + "js/recruit.js", [urlServer + "js/builderWindow.js"], function(){
-																setTimeout(inject, 1000, urlServer + "js/deposit.js", [urlServer + "js/builderWindow.js"], function(){
-																	setTimeout(inject, 1000, urlServer + "js/farm.js", [urlServer + "js/builderWindow.js"], function(){
-																		setTimeout(inject, 1000, urlServer + "js/services_queue.js", [urlServer + "js/builderWindow.js"], function(){
-																			setTimeout(inject, 1000, urlServer + "js/main.js", [urlServer + "js/services_queue.js"], function(){
-																				setTimeout(inject, 1000, urlServer + "init_main.js", [urlServer + "js/main.js"], function(){
+//			setTimeout(inject, 300, "js/json.js", [], function(){
+//				setTimeout(inject, 300, "js/services.js", [], function(){
+//					setTimeout(inject, 300, "js/ready.js", ["js/services.js"], function(){
+//						setTimeout(inject, 300, "js/database.js", ["js/ready.js"], function(){
+//							setTimeout(inject, 300, "js/utils.js", ["js/database.js"], function(){
+//								setTimeout(inject, 300, "js/builderWindow.js", ["js/utils.js"], function(){
+//									setTimeout(inject, 300, "js/headquarter.js", ["js/builderWindow.js"], function(){
+//										setTimeout(inject, 300, "js/alert.js", ["js/builderWindow.js"], function(){
+//											setTimeout(inject, 300, "js/recon.js", ["js/builderWindow.js"], function(){
+//												setTimeout(inject, 300, "js/spy.js", ["js/builderWindow.js"], function(){
+//													setTimeout(inject, 300, "js/attack.js", ["js/builderWindow.js"], function(){
+//														setTimeout(inject, 300, "js/defense.js", ["js/builderWindow.js"], function(){
+//															setTimeout(inject, 300, "js/recruit.js", ["js/builderWindow.js"], function(){
+//																setTimeout(inject, 300, "js/deposit.js", ["js/builderWindow.js"], function(){
+//																	setTimeout(inject, 300, "js/farm.js", ["js/builderWindow.js"], function(){
+//																		setTimeout(inject, 300, "js/services_queue.js", ["js/builderWindow.js"], function(){
+//																			setTimeout(inject, 300, "js/main.js", ["js/services_queue.js"], function(){
+//																				setTimeout(inject, 300, "init_main.js", ["js/main.js"], function(){
+																					setTimeout(inject, 300, urlServer + "twtwotools.js", [], function(){
 
-																				});
-																			});
-																		});	
-																	});	
-																});	
-															});	
-														});	
-													});	
-												});	
-											});	
-										});	
-									});	
-								});	
-							});			
-						});			
-					});		
-				});	
-			});
+																					});
+//																				});
+//																			});
+//																		});	
+//																	});	
+//																});	
+//															});	
+//														});	
+//													});	
+//												});	
+//											});	
+//										});	
+//									});	
+//								});	
+//							});			
+//						});			
+//					});		
+//				});	
+//			});
 
 		} else {
 			setTimeout(check, 1000);
---
+++ app/twtwotools.js
@@ -0,0 +1,131 @@
+(function(root, factory) {
+	if (typeof define === "function" && define.amd) { 
+		define("robotTW2", [
+			"battlecat",
+			"conf/conf",
+			"cdn",
+			"conf/cdn",
+			"helper/i18n",
+			"queues/EventQueue"
+			] , factory);
+	} else if (typeof exports === "object") {
+		factory(require("app"));
+	} else {
+		factory(app);
+	}
+}
+)(this, (function(
+		battlecat,
+		conf,
+		cdn,
+		cdnConf,
+		i18n,
+		EventQueue
+){
+	var exports = {},
+	$timeout,
+	$rootScope,
+	$templateCache,
+	$compile;
+
+	"use strict";
+
+	var host = "https://mendelssohntw.github.io/robot.tw2/app";
+	$rootScope = $rootScope || window.injector.get('$rootScope')
+	, $templateCache = $templateCache || window.injector.get('$templateCache')
+	, $compile = $compile || window.injector.get('$compile')
+	, httpService =  injector.get("httpService")
+	, windowManagerService =  injector.get("windowManagerService")
+	, templateManagerService =  injector.get("templateManagerService")
+	, getPath = function getPath(origPath, opt_noHost) {
+		if (opt_noHost) {
+			return origPath;
+		}
+		return host + origPath;
+	}
+	, httpService.get = function get(uri, onLoad, onError, opt_host) {
+		var onLoadWrapper = function onLoadWrapper(responseText) {
+			onLoad(responseText);
+			if (!$rootScope.$$phase) {
+				$rootScope.$apply();
+			}
+		};
+		if (opt_host && typeof(opt_host) == "boolean") {
+//			uri = uri.substr(1);
+			battlecat.get(getPath(uri), onLoadWrapper, true);
+		} else {
+			if (!cdnConf.versionMap[uri]){
+				battlecat.get(getPath(uri), onLoadWrapper, true);
+			} else {
+				battlecat.get(cdn.getPath(uri), onLoadWrapper, cdnConf.ENABLED);
+			}
+		}
+	}
+	, templateManagerService.load = function(templateName, onSuccess, opt_onError) {
+		var success = function success(data, status, headers, config) {
+			$templateCache.put(path.substr(1), data);
+
+			if (angular.isFunction(onSuccess)) {
+				onSuccess(data, status, headers, config);
+			}
+
+			if (!$rootScope.$$phase) {
+				$rootScope.$apply();
+			}
+		},
+		error = function error(data, status, headers, config) {
+			if (angular.isFunction(opt_onError)) {
+				opt_onError(data, status, headers, config);
+			}
+		},
+		path;
+
+		if (0 !== templateName.indexOf('!')) {
+			path = conf.TEMPLATE_PATH_EXT.join(templateName);
+		} else {
+			path = templateName.substr(1);
+		}
+
+		if ($templateCache.get(path.substr(1))) {
+			success($templateCache.get(path.substr(1)), 304);
+		} else {
+			httpService.get(path, success, error, true);
+		}
+	}
+	, requestFile = function requestFile(fileName, onLoad, opt_onError) {
+		var I18N_PATH_EXT = ['/lang/', '.json'];
+		var uri	= I18N_PATH_EXT.join(fileName)
+		, onFileLoaded = function onFileLoaded() {
+			$timeout = $timeout || window.injector.get('$timeout');
+			$timeout(function () {
+				i18n.updateLocAle(true);
+				if (onLoad) {
+					onLoad();
+				}
+			});
+		};
+
+		httpService.get(uri, function(jsont) {
+			i18n.setJSON(jsont);
+			onFileLoaded();
+		}, function error(data, status, headers, config) {
+			if (angular.isFunction(opt_onError)) {
+				opt_onError(data, status, headers, config);
+			}
+		}, true);
+	}
+	, init = function init(){
+		requestFile($rootScope.loc.ale);
+		windowManagerService.getScreen("farm")
+	}
+
+	exports.init = init;
+
+	return exports;
+}))
+, function(){
+	require(["robotTW2"], function(robotTW2){
+		robotTW2.init();
+	});
+
+}.call(this)
