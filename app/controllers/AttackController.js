bce6df0cefd5e23ed682fbe3b3f6c218ab58c2c8
Mendelssohn <Mendelssohn@DESKTOP-OAMJH0J>
2018-11-06 23:37:00 -0200

    up

--- app/controllers/AttackController.js
+++ app/controllers/AttackController.js
@@ -1,11 +1,136 @@
 define("robotTW2/controllers/AttackController", [
-	"robotTW2"
+	"robotTW2",
+	"robotTW2/databases",
+	"robotTW2/services",
+	"robotTW2/providers",
+	"robotTW2/conf"
 	], function(
-			robotTW2
+			robotTW2,
+			databases,
+			services,
+			providers,
+			conf
 	){
 	return function AttackController($rootScope, $scope) {
 		$scope.CLOSE = services.$filter("i18n")("CLOSE", $rootScope.loc.ale);
+		$scope.CLEAR = services.$filter("i18n")("CLEAR", $rootScope.loc.ale);
 		var self = this;
+		
+		var TABS = {
+				ATTACK 	: services.$filter("i18n")("attack", $rootScope.loc.ale, "attack"),
+				LOG		: services.$filter("i18n")("log", $rootScope.loc.ale, "attack")
+		}
+		, TAB_ORDER = [
+			TABS.ATTACK,
+			TABS.LOG,
+			]
+
+//		$($window.$data.rootnode).addClass("fullsize")
+
+		$scope.requestedTab = TABS.ATTACK;
+		$scope.TABS = TABS;
+		$scope.TAB_ORDER = TAB_ORDER;
+
+		var setActiveTab = function setActiveTab(tab) {
+			$scope.activeTab								= tab;
+			$scope.requestedTab								= null;
+		}
+		, initTab = function initTab() {
+			if (!$scope.activeTab) {
+				setActiveTab($scope.requestedTab);
+			}
+		}
+
+		initTab();
+
+		$scope.data_attack = data_attack.getAttack();
+		$scope.comandos = $scope.data_attack.COMMANDS;
+
+		$scope.userSetActiveTab = function(tab){
+			setActiveTab(tab);
+		}
+
+		$scope.getVstart = function(param){
+			var vid = param.start_village;
+			if(!vid){return}
+			return services.modelDataService.getSelectedCharacter().getVillage(vid).data.name
+		}
+
+		$scope.getVcoordStart = function(param){
+
+			var vid = param.start_village;
+			if(!vid){return}
+			var x = services.modelDataService.getSelectedCharacter().getVillage(vid).data.x
+			var y = services.modelDataService.getSelectedCharacter().getVillage(vid).data.y
+			return "(" + x + "/" + y + ")"
+		}
+
+		$scope.getClass = function(type){
+			var className = "";
+			switch (type) {
+			case "support": {
+				className = "icon-26x26-support";
+				break;
+			}
+			case "attack": {
+				className = "icon-26x26-attack-red";
+				break;
+			}
+			case "relocate": {
+				className = "icon-26x26-relocate";
+				break;
+			}
+			}
+			return className
+		}
+
+		$scope.getHoraSend = function(param){
+
+			return services.$filter("date")(new Date(param.data_escolhida - param.duration), "HH:mm:ss.sss");
+		}
+
+		$scope.getHoraAlvo = function(param){
+
+			return services.$filter("date")(new Date(param.data_escolhida), "HH:mm:ss.sss");
+		}
+
+		$scope.getDataAlvo = function(param){
+
+			return services.$filter("date")(new Date(param.data_escolhida), "dd/MM/yyyy");
+		}
+
+		$scope.getTimeRest = function(param){
+			var difTime = param.data_escolhida - helper.gameTime() - param.duration; 
+			return helper.readableMilliseconds(difTime)
+		}
+
+		$scope.getVcoordTarget = function(param){
+
+			return "(" + param.target_x + "/" + param.target_y + ")"
+		}
+
+		$scope.clear_attack = function(){
+			attack.removeAll();
+		}
+
+		$scope.removeCommand = attack.removeCommandAttack;
+
+		$rootScope.$on(providers.eventTypeProvider.CHANGE_COMMANDS, function() {
+			$scope.data_attack = data_attack.getAttack();
+			$scope.comandos = $scope.data_attack.COMMANDS;
+			if (!$scope.$$phase) {
+				$scope.$apply();
+			}
+		})
+
+		if (!$scope.$$phase) {
+			$scope.$apply();
+		}
+
+		services.$timeout(function(){
+			$window.setCollapse();
+			$window.recalcScrollbar();
+		}, 500)
 
 		return $scope;
 	}
