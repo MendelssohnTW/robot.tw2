d91efa738689c69a83fe7e8543dac4894b28a521
Mendelssohn <Mendelssohn@DESKTOP-OAMJH0J>
2018-11-12 02:38:02 -0200

    up

--- app/controllers/FarmCompletionController.js
+++ app/controllers/FarmCompletionController.js
@@ -28,21 +28,10 @@ define("robotTW2/controllers/FarmCompletionController", [
 		
 		$scope.$watch('reportData', function () {
 			if($rootScope.data_farm.list_exceptions.find(f => f == $scope.reportData.defVillageId)){
-				$("#check-button").removeClass("icon-26x26-dot-red").addClass("icon-26x26-dot-green")
 				$scope.checked = true;
 			} else {
-				$("#check-button").removeClass("icon-26x26-dot-green").addClass("icon-26x26-dot-red")
 				$scope.checked = false;
 			}
-			$("#check-button").click(function () {
-				if ($("#check-button").attr("class")[0] === 'icon-26x26-dot-green') {
-					$(this).removeClass("icon-26x26-dot-green").addClass("icon-26x26-dot-red");
-					removeExcept($scope.reportData.defVillageId);
-				} else {
-					$(this).removeClass("icon-26x26-dot-red").addClass("icon-26x26-dot-green");
-					addExcept($scope.reportData.defVillageId);
-				}
-			});
 		});
 
 		return $scope;
