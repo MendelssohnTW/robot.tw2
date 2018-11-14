acc5a0b4c48bde1a97eed089d23e0a2f607db89e
Mendelssohn <Mendelssohn@DESKTOP-OAMJH0J>
2018-11-14 04:24:31 -0200

    up

--- app/databases/data_villages.js
+++ app/databases/data_villages.js
@@ -38,7 +38,7 @@ define("robotTW2/databases/data_villages", [
 	}
 	db_villages.getAssignedPresets = function(vid){
 		var presetsByVillage = services.modelDataService.getPresetList().presetsByVillage;
-		return presetsByVillage[vid];
+		return Object.keys(presetsByVillage[vid]) || [];
 	}
 	db_villages.verifyVillages = function (villagesExtended){
 		updated = false;
