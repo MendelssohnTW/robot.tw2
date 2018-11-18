0022942cee3299d951cec49b2d5b04a0fb2684fd
Mendelssohn <Mendelssohn@DESKTOP-OAMJH0J>
2018-11-18 05:52:19 -0200

    up

--- app/controllers/DepositController.js
+++ app/controllers/DepositController.js
@@ -1,7 +1,13 @@
 define("robotTW2/controllers/DepositController", [
-	"robotTW2"
+	"robotTW2",
+	"robotTW2/services",
+	"robotTW2/providers",
+	"helper/time",
 	], function(
-			robotTW2
+			robotTW2,
+			services,
+			providers,
+			helper
 	){
 	return function DepositController($rootScope, $scope) {
 		$scope.CLOSE = services.$filter("i18n")("CLOSE", $rootScope.loc.ale);
