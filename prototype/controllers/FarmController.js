93943b682ed0278545d3eab8eed045429cecabef
Mendelssohn <Mendelssohn@DESKTOP-OAMJH0J>
2018-11-23 05:26:52 -0200

    up

--- prototype/controllers/FarmController.js
+++ prototype/controllers/FarmController.js
@@ -206,8 +206,8 @@ define("robotTW2/controllers/FarmController", [
 		
 
 		$scope.$watch("presetSelected", function(){
-			if(!$scope.presetSelected){return}
-			console.log("teste")
+			if(!$scope.presetSelected || !$scope.villageSelected || !$scope.villageSelected[$scope.presetSelected.id]){return}
+			$rootScope.data_villages[$scope.villageSelected].presets[$scope.presetSelected.id] = $scope.presetSelected;
 			if (!$rootScope.$$phase) $rootScope.$apply();
 		}, true)
 
