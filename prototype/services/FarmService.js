3cc25374940e493c78a3c7d61d519c2eced27a81
Mendelssohn Sérgio <mendelssohn.sergio@gmail.com>
2018-11-29 15:37:14 -0200

    Update FarmService.js

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -229,14 +229,14 @@ define("robotTW2/services/FarmService", [
 			, y2 = village.data.y
 
 			var quadrant = 0;
-			if(x2 > x1 && y2 < y1) {
-				quadrant = 3
-			} else if (x2 > x1 && y2 > y1) {
-				quadrant = 2
-			} else if (x2 < x1 && y2 < y1) {
-				quadrant = 4
-			} else if (x2 < x1 && y2 > y1) {
+			if(x1 < x2 && y2 < y1) {
 				quadrant = 1
+			} else if (x1 < x2 && y2 > y1) {
+				quadrant = 4
+			} else if (x1 > x2 && y2 < y1) {
+				quadrant = 2
+			} else if (x1 > x2 && y2 > y1) {
+				quadrant = 3
 			}
 
 			if (y1 % 2) //se y é impar
