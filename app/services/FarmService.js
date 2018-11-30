b84e8f6cb0c7e862945eaad7d631eed18461baf1
mende <mende@DESKTOP-FQRVSE2>
2018-11-30 07:45:20 -0200

    up

--- app/services/FarmService.js
+++ app/services/FarmService.js
@@ -147,10 +147,7 @@ define("robotTW2/services/FarmService", [
 			}
 			if (list_select.length > 0) {
 				list_select.sort(function (a, b) {return a[1] - b[1]});
-//				var m = Math.trunc(($rootScope.data_farm.presets[preset_id].max_journey_time / 60 / 1000 / list_select.pop()[1]) * (bonus / 100) * 0.75);
-				var m = Math.trunc((($rootScope.data_villages.villages[village_id].presets[preset_id].max_journey_time / 60 / 1000 / list_select.pop()[1]) * (bonus / 100) * 0.75)/2);
-				var n = $rootScope.data_villages.villages[village_id].presets[preset_id].max_journey_distance;
-				return Math.min.apply(null, [m, n])
+				return Math.trunc(($rootScope.data_villages.villages[village_id].presets[preset_id].max_journey_time / 60 / 1000 / list_select.pop()[1]) * (bonus / 100) * 0.75);
 			} 
 			return 0;
 		}
