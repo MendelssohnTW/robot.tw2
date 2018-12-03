1af8a1b8eaf553d5e093334be40137f5bbd757f6
mende <mende@DESKTOP-FQRVSE2>
2018-12-03 15:07:44 -0200

    up

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -192,25 +192,14 @@ define("robotTW2/services/FarmService", [
 			, preset_units = cmd_preset.preset_units
 			, aldeia_commands_lenght = countCommands[village_id].length
 			, t_obj = units_analyze(preset_units, aldeia_units);
-			console.log("\n")
-			console.log("comandos da aldeia " + village.data.name + " = " + aldeia_commands_lenght)
-			console.log("unidade preset " + Object.keys(t_obj)[0] + " : " + Object.values(t_obj)[0])
-			
-			console.log("tamanho da lista BB " + lt_bb.length)
-			console.log("cortando em " + ($rootScope.data_villages.villages[village_id].presets[preset_id].max_commands_farm - aldeia_commands_lenght))
 			lt_bb.splice($rootScope.data_villages.villages[village_id].presets[preset_id].max_commands_farm - aldeia_commands_lenght);
-			console.log("tamanho da lista BB " + lt_bb.length)
 			if(t_obj){
-				!t_slice[village_id] ? t_slice[village_id] = Math.trunc(aldeia_units[Object.keys(t_obj)[0]].available / Object.values(t_obj)[0]) : t_slice[village_id];
-				console.log("comandos possiveis para unidade da preset " + t_slice[village_id])
+				!t_slice[village_id] ? t_slice[village_id] = Math.trunc((aldeia_units[Object.keys(t_obj)[0]].available * 0.95) / Object.values(t_obj)[0]) : t_slice[village_id];
 				var d = $rootScope.data_villages.villages[village_id].presets[preset_id].max_commands_farm - aldeia_commands_lenght;
-				console.log("comandos disponíveis para a aldeia " + d)
 				var s = Math.min(d, t_slice[village_id])
 				if(s > 0){
-					console.log("cortando em " + s)
 					t_slice[village_id] = t_slice[village_id] - s;
 					lt_bb.splice(s);
-					console.log("tamanho da lista BB " + lt_bb.length)
 				} else {
 					t_slice[village_id] = 0;
 				}
