d57dffbd250df9e8f6efdcbcf076e06597a0b5e4
mende <mende@DESKTOP-FQRVSE2>
2018-12-07 01:33:04 -0200

    up

--- app/controllers/MapController.js
+++ app/controllers/MapController.js
@@ -3,13 +3,21 @@ define("robotTW2/controllers/MapController", [
 	"robotTW2/providers",
 	"robotTW2/conf",
 	"robotTW2/time",
-	"helper/time"
+	"helper/time",
+	"cdn",
+	"conf/conf",
+	"helper/mapconvert",
+	"struct/MapData"
 	], function(
 			services,
 			providers,
 			conf,
 			convertedTime,
-			helper
+			helper,
+			cdn,
+			conf_conf,
+			mapconvert,
+			MapData
 	){
 	return function MapController($rootScope, $scope) {
 		var data_attack = $rootScope.data_attack;
@@ -18,15 +26,20 @@ define("robotTW2/controllers/MapController", [
 		var self = this;
 
 		var TABS = {
-				MAP 	: services.$filter("i18n")("attack", $rootScope.loc.ale, "attack"),
-				LOG		: services.$filter("i18n")("log", $rootScope.loc.ale, "attack")
+				MINIMAP 	: services.$filter("i18n")("minimap", $rootScope.loc.ale, "map"),
+				HIGHLIGHTS	: services.$filter("i18n")("highlights", $rootScope.loc.ale, "map"),
+				SETTINGS	: services.$filter("i18n")("settings", $rootScope.loc.ale, "map")
 		}
 		, TAB_ORDER = [
-			TABS.MAP,
-			TABS.LOG,
+			TABS.MINIMAP,
+			TABS.HIGHLIGHTS,
+			TABS.SETTINGS
 			]
-
-		$scope.requestedTab = TABS.ATTACK;
+		
+		$scope.colorPalette = ["#000000", "#010067", "#d5ff00", "#ff0056", "#9e008e", "#0e4ca1", "#ffe502", "#005f39", "#00ff00", "#95003a", "#ff937e", "#a42400", "#001544", "#91d0cb", "#620e00", "#6b6882", "#0000ff", "#007db5", "#6a826c", "#00ae7e", "#c28c9f", "#be9970", "#008f9c", "#5fad4e", "#ff0000", "#ff00f6", "#ff029d", "#683d3b", "#ff74a3", "#968ae8", "#98ff52", "#a75740", "#01fffe", "#ffeee8", "#fe8900", "#bdc6ff", "#01d0ff", "#bb8800", "#7544b1", "#a5ffd2", "#ffa6fe", "#774d00", "#7a4782", "#263400", "#004754", "#43002c", "#b500ff", "#ffb167", "#ffdb66", "#90fb92", "#7e2dd2", "#bdd393", "#e56ffe", "#deff74", "#00ff78", "#009bff", "#006401", "#0076ff", "#85a900", "#00b917", "#788231", "#00ffc6", "#ff6e41", "#e85ebe"];
+		$scope.types=  ["village", "character", "tribe"];
+		
+		$scope.requestedTab = TABS.MINIMAP;
 		$scope.TABS = TABS;
 		$scope.TAB_ORDER = TAB_ORDER;
 
@@ -39,95 +52,79 @@ define("robotTW2/controllers/MapController", [
 				setActiveTab($scope.requestedTab);
 			}
 		}
-		, update = function(){
-			$scope.comandos = Object.keys($rootScope.data_attack.commands).map(function(elem, index, array){
-				return $rootScope.data_attack.commands[elem]
-			});
-			$scope.comandos.sort(function(a,b){return (a.data_escolhida - convertedTime() - a.duration) - (b.data_escolhida - convertedTime() - b.duration)})
-			if (!$rootScope.$$phase) {
-				$rootScope.$apply();
-			}
-		}
 
 		initTab();
-		update();
 
 		$scope.userSetActiveTab = function(tab){
 			setActiveTab(tab);
 		}
-
-		$scope.getVstart = function(param){
-			var vid = param.start_village;
-			if(!vid){return}
-			return services.modelDataService.getSelectedCharacter().getVillage(vid).data.name
+		
+		
+		
+		var z = 5
+		, A = 1
+		, F = {}
+		, G = {}
+		, getVillageBlock = function() {
+			return z + A
 		}
-
-		$scope.getVcoordStart = function(param){
-
-			var vid = param.start_village;
-			if(!vid){return}
-			var x = services.modelDataService.getSelectedCharacter().getVillage(vid).data.x
-			var y = services.modelDataService.getSelectedCharacter().getVillage(vid).data.y
-			return "(" + x + "/" + y + ")"
+		, N = function(a) {
+			var b
+			, c = cdn.getPath(conf_conf.getMapPath());
+			b = new XMLHttpRequest,
+			b.open("GET", c, !0),
+			b.responseType = "arraybuffer",
+			b.addEventListener("load", function(c) {
+				a(b.response)
+			}, !1),
+			b.send()
 		}
-
-		$scope.getClass = function(type){
-			var className = "";
-			switch (type) {
-			case "support": {
-				className = "icon-26x26-support";
-				break;
-			}
-			case "attack": {
-				className = "icon-26x26-attack-red";
-				break;
-			}
-			case "relocate": {
-				className = "icon-26x26-relocate";
-				break;
-			}
-			}
-			return className
+		, O = function() {
+			var a
+			, b
+			, c
+			, d = getVillageBlock()
+			, e = Math.round(d / 2);
+			N(function(f) {
+				for (y = new DataView(f),
+						b = 1; b < 999; b++)
+					for (c = 1; c < 999; c++)
+						a = mapconvert.toTile(y, b, c),
+						a.key.b && 
+						(a.key.c 
+								? (context.fillStyle = "rgba(255,255,255,0.8)", context.fillRect(b * d + e - 1, c * d + e - 1, 3, 1), context.fillRect(b * d + e, c * d + e - 2, 1, 3)) 
+								: (context.fillStyle = "rgba(255,255,255,1)", context.fillRect(b * d + e, c * d + e - 1, 1, 1))
+								)
+			})
 		}
+		
+		
+		var canvas = document.createElement("canvas")
+		, context = canvas.getContext("2d")
+		, n = $rootScope.data_map.highlights
+		
+		/*
+		 * interface
+		 */
+		, cross = $(".cross")[0].getContext("2d")
+		, minimap = $(".minimap")[0].getContext("2d")
+		, minimap_window = $("#minimap")[0]
+		
+		//u//
+		, main = document.getElementById("main-canvas")
+		, charModel = services.modelDataService.getSelectedCharacter()
+		, tribeRelationsModel = charModel.getTribeRelations()
+		, D = $rootScope.data_map.cacheVillages;
+		
+		O()
 
-		$scope.getHoraSend = function(param){
-
-			return services.$filter("date")(new Date(param.data_escolhida - param.duration), "HH:mm:ss.sss");
-		}
-
-		$scope.getHoraAlvo = function(param){
-
-			return services.$filter("date")(new Date(param.data_escolhida), "HH:mm:ss.sss");
-		}
-
-		$scope.getDataAlvo = function(param){
-
-			return services.$filter("date")(new Date(param.data_escolhida), "dd/MM/yyyy");
-		}
-
-		$scope.getTimeRest = function(param){
-			var difTime = param.data_escolhida - convertedTime() - param.duration; 
-			return helper.readableMilliseconds(difTime)
-		}
-
-		$scope.getVcoordTarget = function(param){
-
-			return "(" + param.target_x + "/" + param.target_y + ")"
-		}
-
-		$scope.clear_attack = function(){
-			services.AttackService.removeAll();
-		}
-
-		$scope.removeCommand = services.AttackService.removeCommandAttack;
 
 		$rootScope.$on(providers.eventTypeProvider.CHANGE_COMMANDS, function() {
 			update();
 			
 		})
 
-		$scope.setCollapse();
-		$scope.recalcScrollbar();
+		$scope.disableScrollbar();
 
 		return $scope;
 	}
