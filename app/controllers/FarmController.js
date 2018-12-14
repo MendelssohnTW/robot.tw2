7997047128f4fbdad0746299337bcd33370818a5
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-14 11:09:06 -0200

    up

--- app/controllers/FarmController.js
+++ app/controllers/FarmController.js
@@ -107,7 +107,7 @@ define("robotTW2/controllers/FarmController", [
 			};
 			for (presetId in $scope.data.presets) {
 				$scope.data.presets[presetId] = angular.merge({}, $rootScope.data_villages.villages[$scope.villageSelected.data.villageId].presets[presetId])
-				if(!$scope.data.presets[presetId].assigned_villages.length){continue}
+				if(!$scope.data.presets[presetId].assigned_villages){continue}
 				$scope.data.presets[presetId].assigned_villages.forEach(assignPreset);
 			}
 			if(typeof(callback) == "function"){
