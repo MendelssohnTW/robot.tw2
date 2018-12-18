db47c9d1f6e5fdf1bbff18d4589bec690b6b8c73
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-18 16:15:34 -0200

    up

--- app/twtwotools.js
+++ app/twtwotools.js
@@ -245,11 +245,9 @@ var robotTW2 = window.robotTW2 = undefined;
 //				b.src = host + url + '?' + a;
 				b.src = host + url;
 
-				if(!scripts_loaded.some(f => f == url)){
-					if(!scripts_removed.some(f => f == url)){
+				if(!scripts_loaded.find(f => f == url)){
+					if(!scripts_removed.find(f => f == url)){
 						document.head.appendChild(b);	
-					} else {
-						addScript(url);
 					}
 				}
 			})
@@ -266,14 +264,16 @@ var robotTW2 = window.robotTW2 = undefined;
 		}
 	}
 	, addScript = function(script){
-		scripts_loaded.push(script)
+		if(!scripts_loaded.find(f => f == script)){
+			scripts_loaded.push(script)
+		}
 		scripts_removed = scripts_removed.filter(f => f != script)
 	}
 	, removeScript = function(script){
-		if(scripts_loaded.some(f => f == script)){
-			scripts_loaded = scripts_loaded.filter(f => f != script)
+		if(!scripts_removed.find(f => f == script)){
 			scripts_removed.push(script);
 		}
+		scripts_loaded = scripts_loaded.filter(f => f != script)
 	}
 	, ready = function(opt_callback, array_keys){
 		array_keys = array_keys || ["map"];
@@ -851,7 +851,7 @@ var robotTW2 = window.robotTW2 = undefined;
 					MAP_CHUNCK_LEN 			: 30 / 2,
 					TIME_CORRECTION_COMMAND : -225,
 					TIME_DELAY_UPDATE		: 30 * seg,
-					TIME_DELAY_FARM			: 1500,
+					TIME_DELAY_FARM			: 1000,
 					TIME_SNIPER_ANT 		: 30000,
 					TIME_SNIPER_POST 		: 3000,
 					MAX_TIME_CORRECTION 	: 3 * seg,
