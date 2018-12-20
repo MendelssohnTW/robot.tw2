c452c73934cc851125dc15eb2606898431f65d8b
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-20 04:25:30 -0200

    up

--- prototype/twtwotools.js
+++ prototype/twtwotools.js
@@ -1071,7 +1071,9 @@ var robotTW2 = window.robotTW2 = undefined;
 				var opt_callback = callbacks[id_return];
 				opt_callback(msg);
 			},
-			onclose = function onclose(){
+			onclose = function onclose($event, data){
+				console.log($event)
+				console.log(data)
 			},
 			onerror = function onerror($event){
 				if($rootScope.data_data){
