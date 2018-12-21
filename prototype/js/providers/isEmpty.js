555594cc8791b159d56bc493d873ac9b264420ac
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-21 20:57:02 -0200

    up

--- prototype/js/providers/isEmpty.js
+++
@@ -1,9 +0,0 @@
-define([
-	"app.providers"
-	], function (
-			factories
-	) {	
-	factories.factory("isEmpty", function (){
-		return function(obj){obj ? Object.keys(obj).length === 0 : 0}
-	})
-});
