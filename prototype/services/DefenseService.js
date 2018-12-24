8975c646b31c407cefa21749c254090fa88810f3
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-24 12:02:40 -0200

    up

--- prototype/services/DefenseService.js
+++ prototype/services/DefenseService.js
@@ -332,7 +332,7 @@ define("robotTW2/services/DefenseService", [
 									target_x			: aldeia.x,
 									target_y			: aldeia.y,
 									type				: "support",
-									data_escolhida		: time.convertMStoUTC(helper.unreadableSeconds(helper.readableMilliseconds(cmt.completedAt, false))),
+									data_escolhida		: time.convertMStoUTC(cmt.completedAt),
 									time_sniper_ant		: $rootScope.data_defense.time_sniper_ant,
 									time_snipe_ant		: timeSniperPost,
 									no_target			: false
