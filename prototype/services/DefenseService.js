50e305f3e424687958406160730c908933fed699
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-26 11:52:05 -0200

    up

--- prototype/services/DefenseService.js
+++ prototype/services/DefenseService.js
@@ -333,9 +333,10 @@ define("robotTW2/services/DefenseService", [
 									data_escolhida		: time.convertMStoUTC(cmt.completedAt),
 									time_sniper_ant		: $rootScope.data_defense.time_sniper_ant,
 									time_sniper_post	: timeSniperPost,
-									no_target			: false
+									no_target			: false,
+									id_command 			: cmt.id
 							}
-							addDefense(params, cmt.id);
+							addDefense(params);
 						}
 						ct();
 					});
@@ -572,7 +573,7 @@ define("robotTW2/services/DefenseService", [
 				return null
 			}
 		}
-		, addDefense = function(params, opt_id){
+		, addDefense = function(params){
 //			if(params && id_command){
 //			var t = {
 //			id_command: id_command
@@ -601,7 +602,7 @@ define("robotTW2/services/DefenseService", [
 
 			if(!params){return}
 			var id_command = (Math.round(time.convertedTime() / 1e9)).toString() + params.data_escolhida.toString();
-			if(opt_id){
+			if(params.id_command){
 				id_command = params.id_command
 			}
 
@@ -668,9 +669,10 @@ define("robotTW2/services/DefenseService", [
 									data_escolhida		: time.convertMStoUTC(cmt.model.completedAt),
 									time_sniper_ant		: sniper_ant * 1000,
 									time_sniper_post	: sniper_post * 1000,
-									no_target			: true
+									no_target			: true,
+									id_command			: cmt.command_id
 							}
-							addDefense(params, cmt.command_id);
+							addDefense(params);
 						}
 					})
 				} else {
