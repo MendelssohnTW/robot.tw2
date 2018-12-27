84e642bceb13f7bafda48c235cd79bd77e9667e2
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2018-12-27 21:46:43 -0200

    up

--- prototype/services/AttackService.js
+++ prototype/services/AttackService.js
@@ -97,7 +97,7 @@ define("robotTW2/services/AttackService", [
 		, resendAttack = function(params){
 //			var data_main = robotTW2.databases.data_main.get()
 			var id_command = params.id_command
-			var expires_send = params.data_escolhida - params.duration - $rootScope.data_main.time_correction_command;
+			var expires_send = params.data_escolhida - params.duration + $rootScope.data_main.time_correction_command;
 			var timer_delay_send = expires_send - time.convertedTime();
 			if(timer_delay_send >= 0){
 				function e (){
