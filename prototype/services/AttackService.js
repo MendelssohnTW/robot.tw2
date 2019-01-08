dd613f9cfee2bb8e175a39bcc856e31a222d7964
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-01-08 20:44:17 -0200

    up

--- prototype/services/AttackService.js
+++ prototype/services/AttackService.js
@@ -129,7 +129,7 @@ define("robotTW2/services/AttackService", [
 			return $timeout(units_to_send.bind(null, params), params.timer_delay - conf.TIME_DELAY_UPDATE)
 		}
 		, resendAttack = function(id_command){
-			if(!scope.params[id_command]){console.log("no params");return}
+			if(!scope.params[id_command]){console.log("no params resend");return}
 			var expires_send = scope.params[id_command].data_escolhida - scope.params[id_command].duration - $rootScope.data_main.time_correction_command
 			, timer_delay_send = expires_send - time.convertedTime();
 
