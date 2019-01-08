a99a53bd354980314eea52e0601023a96eeb975a
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-01-08 16:45:13 -0200

    up

--- prototype/services/AttackService.js
+++ prototype/services/AttackService.js
@@ -109,6 +109,7 @@ define("robotTW2/services/AttackService", [
 			}
 		}
 		, send = function(id_command){
+			if(!scope.params[id_command]){return}
 			socketService.emit(
 					providers.routeProvider.SEND_CUSTOM_ARMY, {
 						start_village		: scope.params[id_command].start_village,
@@ -128,7 +129,7 @@ define("robotTW2/services/AttackService", [
 			return $timeout(units_to_send.bind(null, params), params.timer_delay - conf.TIME_DELAY_UPDATE)
 		}
 		, resendAttack = function(id_command){
-			if(!scope.params[id_command].data_escolhida){return}
+			if(!scope.params[id_command]){return}
 			var expires_send = scope.params[id_command].data_escolhida - scope.params[id_command].duration - $rootScope.data_main.time_correction_command
 			, timer_delay_send = expires_send - time.convertedTime();
 
