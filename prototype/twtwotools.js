549164b9eb6b0ba5a5e9d4b057ff2a4e34f011c1
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-01-11 22:47:09 -0200

    up

--- prototype/twtwotools.js
+++ prototype/twtwotools.js
@@ -151,7 +151,7 @@ var robotTW2 = window.robotTW2 = undefined;
 					!opt_db.commands[key] ? opt_db.commands[key] = params : null;
 					$rootScope.$broadcast(exports.providers.eventTypeProvider.CHANGE_COMMANDS)
 				} else if(opt_db == "commands_defense"){
-					if(!exports.commands_defense.find(f => f.id_command = parms.id_command)){
+					if(!exports.commands_defense.find(f => f.id_command = params.id_command)){
 						exports.commands_defense.push(params)
 					}
 				}
