29eb1017b7f25cefe4d030669bd1de4526b7fa60
Mendelssohn Sérgio <mendelssohn.sergio@gmail.com>
2019-01-13 18:27:35 -0200

    Update twtwotools.js

--- app/twtwotools.js
+++ app/twtwotools.js
@@ -2083,7 +2083,7 @@ var robotTW2 = window.robotTW2 = undefined;
 								robotTW2.loadScript("/databases/data_recruit.js");
 //								robotTW2.loadScript("/databases/data_medic.js");
 								robotTW2.loadScript("/databases/data_secondvillage.js");
-//								robotTW2.loadScript("/databases/data_data.js");
+								robotTW2.loadScript("/databases/data_data.js");
 //								robotTW2.loadScript("/databases/data_map.js");
 								robotTW2.loadScript("/databases/data_logs.js");
 
