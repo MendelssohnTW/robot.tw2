398af770f4bb0b70a16089e30a81fdaa21a6b013
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-01-17 06:39:35 -0200

    up

--- prototype/databases/data_villages.js
+++ prototype/databases/data_villages.js
@@ -138,7 +138,11 @@ define("robotTW2/databases/data_villages", [
 							buildinglevels 			: conf.BUILDINGLEVELS,
 							farm_activate 			: true,
 							presets					: getPst(m),
-							selected				: elem
+							selected				: {
+								id: 0,
+								name: services.$filter("i18n")(elem, services.$rootScope.loc.ale, "headquarter"),
+								value: elem
+							}
 						})
 					}
 					angular.extend(data_villages.villages[m], villagesExtended[m])
