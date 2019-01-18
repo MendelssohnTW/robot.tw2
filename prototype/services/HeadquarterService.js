8824d3e8ccc9cff2046772150418b44742b46961
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-01-18 08:42:01 -0200

    up

--- prototype/services/HeadquarterService.js
+++ prototype/services/HeadquarterService.js
@@ -191,11 +191,11 @@ define("robotTW2/services/HeadquarterService", [
 					resolve();
 				}
 
-				$rootScope.data_villages.villages[village_id].buildinglevels[$rootScope.data_villages.villages[village_id].selected.value] = buildingLevels;
+				$rootScope.data_villages.villages[village_id].buildinglevels = buildingLevels;
 				if (queues.length) {
 					queues.forEach(
 							function(queue) {
-								$rootScope.data_villages.villages[village_id].buildinglevels[$rootScope.data_villages.villages[village_id].selected.value].map(function(value){
+								$rootScope.data_villages.villages[village_id].buildinglevels.map(function(value){
 									Object.keys(value)[0] == queue.building ? value[queue.building]++ :undefined;
 								})
 							}
