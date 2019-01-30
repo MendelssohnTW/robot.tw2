acbeb2482e6c52793dfb13e78a835a7c555ad650
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-01-30 08:13:17 -0200

    up

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -128,8 +128,8 @@ define("robotTW2/services/FarmService", [
 			var grid = setupGrid(t_ciclo);
 			for (var i = 0; i < t_ciclo; i++) {
 				for (var j = 0; j < t_ciclo; j++) {
-					if(villages_town.load(coordX, (coordX + (t_ciclo * map_chunk_size)), coordY, (coordY + (t_ciclo * map_chunk_size)))){
-						grid[i][j] = {"x":coordX + (map_chunk_size * i), "y":coordY + (map_chunk_size * j), "dist": map_chunk_size};	
+					if(villages_town.load(coordX + (map_chunk_size * i), coordX + (map_chunk_size * (i + 1)), coordY + (map_chunk_size * j), coordY + (map_chunk_size * (j + 1)))){
+						grid[i][j] = {"x": coordX + (map_chunk_size * i), "y": coordY + (map_chunk_size * j), "dist": map_chunk_size};	
 					}
 				};
 			};
