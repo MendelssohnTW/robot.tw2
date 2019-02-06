6d733989041d55833fd4ab9c65365037a1f93628
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-02-06 20:36:00 -0200

    up

--- prototype/services/DefenseService.js
+++ prototype/services/DefenseService.js
@@ -630,7 +630,7 @@ define("robotTW2/services/DefenseService", [
 					console.log("data")
 					console.log(data)
 					var expires = cmd.data_escolhida - (data.time_start * 1000) + cmd.time_sniper_post
-					, timer_delay = ((expires - time.convertedTime()) / 2) + robotTW2.databases.data_main.time_correction_command
+					, timer_delay = (expires / 2) + robotTW2.databases.data_main.time_correction_command
 					, params = {
 						"timer_delay" 	: timer_delay,
 						"id_command" 	: data.id
