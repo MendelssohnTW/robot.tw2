e2ad07641b79dce7a85cb9da80eb6e7b781bca3a
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-02-28 01:52:19 -0300

    up

--- prototype/controllers/SpyController.js
+++ prototype/controllers/SpyController.js
@@ -106,6 +106,7 @@ define("robotTW2/controllers/SpyController", [
 		$scope.isRunning = services.SpyService.isRunning();
 
 		$scope.autoCompleteKeyUp = function(event){
+			event.type = ["character", "tribe", "village"]
 			autocomplete(event)
 		}
 
