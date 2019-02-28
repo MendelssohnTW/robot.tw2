b575bf1069da86912d534f048050f376020f358b
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-02-28 04:26:11 -0300

    up

--- prototype/controllers/SpyController.js
+++ prototype/controllers/SpyController.js
@@ -104,7 +104,7 @@ define("robotTW2/controllers/SpyController", [
 		$scope.isRunning = services.SpyService.isRunning();
 
 		$scope.autoCompleteKeyUp = function(event){
-			var scope = angular.element($("[ng-keyup]")).scope()
+			var scope = angular.element($("#my-ng-keyup")).scope()
 			scope.autoCompleteKeyUp(event)
 		}
 
