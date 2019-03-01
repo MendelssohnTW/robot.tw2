3b9684dad802f54bfb6190c390f3f7df6d9229ee
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-01 19:03:43 -0300

    up

--- app/services/FarmService.js
+++ app/services/FarmService.js
@@ -662,46 +662,22 @@ define("robotTW2/services/FarmService", [
 
 				$rootScope.$broadcast(providers.eventTypeProvider.ISRUNNING_CHANGE, {name:"FARM"})
 
-				if ((data_farm.farm_time_stop - time.convertedTime()) - data_farm.farm_time > 0) {
-					var tempo_delay = data_farm.farm_time_start - time.convertedTime();
-					if(tempo_delay < 0) {
-						tempo_delay = 0
-					} else {
-						$rootScope.$broadcast(providers.eventTypeProvider.MESSAGE_DEBUG, {message: $filter("i18n")("wait_init", $rootScope.loc.ale, "farm")})
-						data_log.farm.push({"text":$filter("i18n")("wait_init", $rootScope.loc.ale, "farm"), "date": (new Date(time.convertedTime())).toString()})
-						data_log.set()
-					};
-					interval_init = $timeout(function () {
+				
 						var init_first = true;
 						var f = function(){
-							if(time.convertedTime() + data_farm.farm_time < data_farm.farm_time_stop){
-								var tempo = 0;
-								if(!init_first){
-									tempo = Math.round((data_farm.farm_time / 2) + (data_farm.farm_time * Math.random()));
-								}
+						
+								var tempo = 60*60*15*1000;
+								
 								init_first = false;
 								execute_cicle(tempo).then(function(){
 									data_log.farm.push({"text":$filter("i18n")("terminate_cicles", $rootScope.loc.ale, "farm"), "date": (new Date(time.convertedTime())).toString()})
 									data_log.set()
 									f()
 								})
-							} else {
-								clear()
-								data_log.farm.push({"text":$filter("i18n")("terminate_cicles", $rootScope.loc.ale, "farm"), "date": (new Date(time.convertedTime())).toString()})
-								data_log.set()
-							}
+							
 						}
 						f()
 
-					}, tempo_delay);
-					return;
-				} else {
-					isRunning = !1;
-					$rootScope.$broadcast(providers.eventTypeProvider.MESSAGE_ERROR, {message: $filter("i18n")("farm_no_init", $rootScope.loc.ale, "farm")})
-					data_log.farm.push({"text":$filter("i18n")("farm_no_init", $rootScope.loc.ale, "farm"), "date": (new Date(time.convertedTime())).toString()})
-					data_log.set()
-					return;
-				}
 			}, ["all_villages_ready"])
 		}
 		, init = function (bool) {
