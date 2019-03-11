8f8442caf3aeeb501cb0ab0992d8c8b005c33baa
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-11 19:36:46 -0300

    uop

--- prototype/controllers/ReconController.js
+++ prototype/controllers/ReconController.js
@@ -18,7 +18,7 @@ define("robotTW2/controllers/ReconController", [
 		$scope.text_version = $scope.version + " " + data_recon.version;
 		
 		$scope.getKey = function(unit_name){
-			return services.$filter("i18n")(unit_name, services.$rootScope.loc.ale, "recon");
+			return services.$filter("i18n")(unit_name, services.$rootScope.loc.ale);
 		}
 		
 		$scope.getClass = function(unit_name){
