ff6ad203768ce27edd65a5068f3c75cc9cb03834
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-12 04:16:41 -0300

    up

--- prototype/controllers/FarmController.js
+++ prototype/controllers/FarmController.js
@@ -118,7 +118,7 @@ define("robotTW2/controllers/FarmController", [
 				'barbarian'		: true
 			})
 
-			return services.armyService.getTravelTimeForDistance(army, travelTime, distance, "attack") * 1000 * 2
+			return services.armyService.getTravelTimeForDistance(army, travelTime, distance, "attack") * 1000
 		}
 		, triggerUpdate = function triggerUpdate() {
 			presetIds = [];
