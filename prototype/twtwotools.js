49c65134dfddd1edea4af43a013cf92f1591d911
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-22 04:07:50 -0300

    up

--- prototype/twtwotools.js
+++ prototype/twtwotools.js
@@ -764,14 +764,12 @@ var robotTW2 = window.robotTW2 = undefined;
 	require(["robotTW2"], function(robotTW2){
 
 		define("robotTW2/getJSON", function getJSON(){
+			var json = {};
 			return function(str){
 				robotTW2.requestFile(str, "/json/", function(jsont){
-					if(str!="conf"){
-						return {[str.toUpperCase()]: jsont}
-					} else {
-						return jsont
-					}
+					angular.extend(new json, jsont)
 				})
+				return json
 			}
 		})
 
