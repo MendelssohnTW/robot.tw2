d4b382ed95ea7cdfee5fd045e6a2759edc630360
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-21 22:06:11 -0300

    up

--- prototype/databases/data_villages.js
+++ prototype/databases/data_villages.js
@@ -172,6 +172,7 @@ define("robotTW2/databases/data_villages", [
 			}
 			db_villages.set();
 		}, function(){
+			console.log(conf)
 			if(!data_villages.version || (typeof(data_villages.version) == "number" ? data_villages.version.toString() : data_villages.version) < conf.VERSION.VILLAGES){
 				data_villages = {};
 				data_villages.version = conf.VERSION.VILLAGES
