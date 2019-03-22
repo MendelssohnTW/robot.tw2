19a10d72f4b762dc78ed2a1fd0e1c2b03b934314
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-21 23:40:26 -0300

    up

--- app_v_1.00/twtwotools.js
+++ app_v_1.00/twtwotools.js
@@ -767,15 +767,17 @@ var robotTW2 = window.robotTW2 = undefined;
 		define("robotTW2/getJSON", function getJSON(){
 			var service = {};
 			return service.getJSON = function(str){
-				var json = {};
 				robotTW2.requestFile(str, "/json/", function(jsont){
-					if(typeof(jsont == "object")){
-						angular.extend(json, jsont)
-					} else {
-						json = jsont
-					}
+					return jsont
 				})
-				return json
+				
+//				robotTW2.requestFile(str, "/json/", function(jsont){
+//					if(Object.keys(jsont).length){
+//						angular.extend(json_object, jsont)
+//					} else if(jsont.length){
+//						json_array = jsont
+//					}
+//				})
 			}
 		})
 
