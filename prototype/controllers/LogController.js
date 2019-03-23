d7884f03fad5ee4e1d8bfac9115968537638b482
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-23 10:59:22 -0300

    up

--- prototype/controllers/LogController.js
+++ prototype/controllers/LogController.js
@@ -14,26 +14,21 @@ define("robotTW2/controllers/LogController", [
 		$scope.MENU = services.$filter("i18n")("MENU", services.$rootScope.loc.ale);
 		$scope.version = services.$filter("i18n")("version", services.$rootScope.loc.ale);
 
+		$scope.data_main = data_main;
+		$scope.data_log = data_log;
+		$scope.extensions = $scope.data_main.getExtensions()
+
 		var self = this
-		, TABS = {
-				ATTACK 			: services.$filter("i18n")("title", services.$rootScope.loc.ale, "attack"),
-				DEFENSE 		: services.$filter("i18n")("title", services.$rootScope.loc.ale, "defense"),
-				DEPOSIT 		: services.$filter("i18n")("title", services.$rootScope.loc.ale, "deposit"),
-				FARM 			: services.$filter("i18n")("title", services.$rootScope.loc.ale, "farm"),
-				HEADQUARTER 	: services.$filter("i18n")("title", services.$rootScope.loc.ale, "headquarter"),
-				RECRUIT 		: services.$filter("i18n")("title", services.$rootScope.loc.ale, "recruit"),
-				SECONDVILLAGE	: services.$filter("i18n")("title", services.$rootScope.loc.ale, "secondvillage"),
-				SPY				: services.$filter("i18n")("title", services.$rootScope.loc.ale, "spy"),
+		, TABS = {};
+		for (var name in $scope.extensions) {
+			if($scope.extensions[name].activated){
+				TABS[name.toUpperCase()] = services.$filter("i18n")("title", services.$rootScope.loc.ale, name.toLowerCase())
+			}			
 		}
-		, TAB_ORDER = [
-			TABS.ATTACK,
-			TABS.DEFENSE,
-			TABS.DEPOSIT,
-			TABS.HEADQUARTER,
-			TABS.RECRUIT,
-			TABS.SECONDVILLAGE,
-			TABS.SPY
-			]
+
+		var TAB_ORDER = Object.keys(TABS).map(function(elem){
+			return TABS[elem]
+		}).sort(function(a,b){return a.localeCompare(b)})
 		, setActiveTab = function setActiveTab(tab) {
 			$scope.activeTab	= tab;
 			$scope.requestedTab	= null;
@@ -43,11 +38,11 @@ define("robotTW2/controllers/LogController", [
 				setActiveTab($scope.requestedTab);
 			}
 		}
-		
+
 		$scope.userSetActiveTab = function(tab){
 			setActiveTab(tab);
 		}
-		
+
 		$scope.requestedTab = TABS.ATTACK;
 		$scope.TABS = TABS;
 		$scope.TAB_ORDER = TAB_ORDER;
