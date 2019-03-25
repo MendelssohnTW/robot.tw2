e39f19345838f4e6cb393f794b7ab75b39a40af6
Mendelssohn Sérgio <mendelssohn.sergio@gmail.com>
2019-03-25 03:20:40 -0300

    Update FarmService.js

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -194,21 +194,16 @@ define("robotTW2/services/FarmService", [
 			return [f.length, aldeia_units] 
 		}
 		, check_commands = function(cmd, village_id, preset_id, cicle){
-			let lt = true;
-			let sum = 0;
-			if(Object.values(countCommands[cicle][village_id]).length)
-				sum = Object.values(countCommands[cicle][village_id]).map(function(elem){return elem}).reduce(function(a, b) {
-					return a.concat(b);
-				}).length;
-			if((cmd.data.direction=="forward") && sum && sum < data_villages.villages[village_id].presets[preset_id].max_commands_farm){
-				lt = Object.keys(countCommands[cicle][village_id]).map(function(pst){
-					return !countCommands[cicle][village_id][pst].some(f=>f==cmd.targetVillageId)
-				}).every(f=>f==true)
-			} else {
-				lt = true;
-			}
-			return lt
-		}
+    let lt = true
+    if(cmd.data.direction!="forward" || d.data.type!="attack"){
+      return false
+    }
+    
+    lt = Object.keys(countCommands[cicle][village_id]).map(function(pst){
+      return !countCommands[cicle][village_id][pst].some(f=>f==cmd.targetVillageId)
+    }).every(f=>f==true)
+  return lt
+}
 		, check_commands_for_bb = function(bb, cicle){
 			let lt = false;
 			if(Object.keys(countCommands[cicle]).length)
