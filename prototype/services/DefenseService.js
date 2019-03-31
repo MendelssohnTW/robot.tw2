497ac78dfbd02459b3e8c5646d5826cce55b5074
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-03-31 10:29:38 -0300

    up

--- prototype/services/DefenseService.js
+++ prototype/services/DefenseService.js
@@ -612,9 +612,9 @@ define("robotTW2/services/DefenseService", [
 					"id_command": id_command
 				})
 
-				console.log("sendDefense bind")
+				console.log("sendDefense bind " + params)
 				commandQueue.bind(params.id_command, sendDefense, null, params, function(fns){
-					console.log("sendDefense trigger")
+					console.log("sendDefense trigger " + params)
 					commandDefense[params.id_command] = {
 							"timeout" 	: fns.fn.apply(this, [fns.params]),
 							"params"	: params
