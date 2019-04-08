13d736269d6a5b3577df94518588fd90d75e1675
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-04-08 18:54:21 -0300

    up

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -85,9 +85,6 @@ define("robotTW2/services/FarmService", [
 			return Object.keys(units).some(unit => unit == unit_search && units[unit].available != undefined && units[unit].available >= 2)
 		}
 		, units_analyze = function (preset_units, aldeia_units, opt) {
-			/*
-			 * Verificar se há nobres e retornar false se haver
-			 */
 			var f = Object.keys(preset_units).map(function(unit_preset){
 				if(preset_units[unit_preset] >= 2 && !data_farm.troops_not.some(elem => elem == unit_preset)) {
 					if (units_has_unit_search(unit_preset, aldeia_units) && aldeia_units[unit_preset].available >= preset_units[unit_preset]) {
