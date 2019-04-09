ca89e4a134b7ac6e5df3157aea9c57ba0924792c
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-04-09 00:14:47 -0300

    up

--- prototype/services/RecruitService.js
+++ prototype/services/RecruitService.js
@@ -242,7 +242,7 @@ define("robotTW2/services/RecruitService", [
 				if(!!data_villages.villages[vls[elem].getId()].recruit_activate && tam < data_recruit.reserva.slots){
 					return vls[elem].getId()
 				} else {
-					console.log(vls[elem].getName() + " tam " + tam)
+					console.log(vls[elem].getName() + " tam " + tam + " activate " + data_villages.villages[vls[elem].getId()].recruit_activate)
 				}
 			}).filter(f=>f!=undefined)
 
