fd5a9b1994ac1fc28613300396a9dcf340f33d2c
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-04-10 06:42:10 -0300

    up

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -231,7 +231,7 @@ define("robotTW2/services/FarmService", [
 							g = undefined;
 							promise_send = undefined;
 							let tot = Object.keys(countCommands[cicle][village_id]).reduce(function(a, b){
-								return countCommands[cicle][village_id][a].length + countCommands[cicle][village_id][b].length
+								return countCommands[cicle][village_id][a].length ? countCommands[cicle][village_id][a].length : 0 + countCommands[cicle][village_id][b] ? countCommands[cicle][village_id][b].length : 0
 							})
 							, parc = countCommands[cicle][village_id][preset_id].length
 							if(promise_send_queue.length && permited && parc <= cmd_ind && tot <= max_cmds){
