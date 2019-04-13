c5f9aa33b1f8c40392d432dd19349a0628cdab2d
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-04-12 21:19:17 -0300

    up

--- prototype/controllers/LogController.js
+++ prototype/controllers/LogController.js
@@ -23,7 +23,7 @@ define("robotTW2/controllers/LogController", [
 		var self = this
 		, TABS = {};
 		for (var name in $scope.extensions) {
-			if($scope.extensions[name].activated && !name.includes["LOG", "RECON"]){
+			if($scope.extensions[name].activated && !name.includes["LOG", "RECON", "ALERT"]){
 				TABS[name.toUpperCase()] = services.$filter("i18n")("title", services.$rootScope.loc.ale, name.toLowerCase())
 			}			
 		}
