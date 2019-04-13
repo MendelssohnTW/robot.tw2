b3b3a982e24c486398d9fde0bc7809a92468fb58
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-04-13 00:49:36 -0300

    up

--- prototype/services/FarmService.js
+++ prototype/services/FarmService.js
@@ -237,10 +237,11 @@ define("robotTW2/services/FarmService", [
 											army_preset_id: cmd_preset_internal.preset_id,
 											type: "attack"
 									}
-
-									let text = $filter("i18n")("text_preset", $rootScope.loc.ale, "farm") +	": " + params.army_preset_id +
-									$filter("i18n")("text_origin", $rootScope.loc.ale, "farm") + ": " + params.start_village + 
-									$filter("i18n")("text_target", $rootScope.loc.ale, "farm") + ": " + params.target_village
+									
+									let pstr = Object.keys(modelDataService.getPresetList().presets).map(function(elem){return modelDataService.getPresetList().presets[elem]}).find(f=>f.id==7993285).name
+									, text = $filter("i18n")("text_preset", $rootScope.loc.ale, "farm") +	": " + pstr +
+									$filter("i18n")(" text_origin", $rootScope.loc.ale, "farm") + ": " + modelDataService.getVillage(params.start_village).getName() + 
+									$filter("i18n")(" text_target", $rootScope.loc.ale, "farm") + ": " + bb.name
 									data_log.farm.push({"text":text, "date": (new Date(time.convertedTime())).toString()})
 									data_log.set()
 
