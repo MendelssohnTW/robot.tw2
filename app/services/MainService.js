254d7c9c18fea69e8fe2703ded1916ff26a341c0
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-05-08 16:53:23 -0300

    up

--- app/services/MainService.js
+++ app/services/MainService.js
@@ -35,7 +35,7 @@ define("robotTW2/services/MainService", [
 		}
 		
 		var verifyConnection = function verifyConnection (){
-			if(document.querySelector('[ng-controller=ModalSocketController]').length){
+			if(document.querySelector('[ng-controller=ModalSocketController]')){
 				timeout = undefined;
 				onErrorTimeout()
 			}
