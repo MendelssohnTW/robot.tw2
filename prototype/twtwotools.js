03fe42da064ee116d12ed4d0d289e969e2cf68f9
mendelssohnsergio <mendelssohn.sergio@gmail.com>
2019-06-17 23:01:22 -0300

    up

--- prototype/twtwotools.js
+++ prototype/twtwotools.js
@@ -25,7 +25,6 @@ var robotTW2 = window.robotTW2 = undefined;
 	var exports = {}
 
 	"use strict";
-
 //	var host = "https://mendelssohntw.github.io/robot.tw2/prototype";
 //	var host = "https://laverel.com/script/prototype";
 //	var host = "https://delacasa.com.py/script/prototype";
